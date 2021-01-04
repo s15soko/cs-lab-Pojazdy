f7c271f48ebadbc463001b5ad6c3fe003ecf9536
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-01-04 17:19:05 +0100

    Add files for console app and lib app

---
+++ VehiclesConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VehiclesConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
