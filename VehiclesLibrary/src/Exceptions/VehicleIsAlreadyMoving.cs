44dc7cac14c96e1bd1085da91e193915483e0eb5
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-01-04 19:04:57 +0100

    Vehicle exceptions

---
+++ VehiclesLibrary/src/Exceptions/VehicleIsAlreadyMoving.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VehiclesLibrary.src.Exceptions
+{
+    public class VehicleIsAlreadyMoving : Exception
+    {
+        public VehicleIsAlreadyMoving() { }
+
+        public VehicleIsAlreadyMoving(string message) : base(message)
+        { }
+
+        public VehicleIsAlreadyMoving(string message, Exception inner) : base(message, inner)
+        { }
+    }
+}
---
+++ VehiclesLibrary/src/Exceptions/VehicleIsAlreadyStanding.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VehiclesLibrary.src.Exceptions
+{
+    public class VehicleIsAlreadyStanding : Exception
+    {
+        public VehicleIsAlreadyStanding() { }
+
+        public VehicleIsAlreadyStanding(string message) : base(message)
+        { }
+
+        public VehicleIsAlreadyStanding(string message, Exception inner) : base(message, inner)
+        { }
+    }
+}
