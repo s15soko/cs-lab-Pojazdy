1008be4535d07e76fb9b9eace72959f04296ffc5
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-01-04 19:58:24 +0100

    Amphibian vehicle

--- VehiclesLibrary/src/Vehicles/Amphibian.cs
+++ VehiclesLibrary/src/Vehicles/Amphibian.cs
@@ -1,14 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using VehiclesLibrary.src.Enums;
 
 namespace VehiclesLibrary.src.Vehicles
 {
-    public class Amphibian
+    public class Amphibian : VehicleBase
     {
-        public Amphibian()
-        {
+        public double Displacement { get; }
+        public int NumberOfWheels { get; }
 
+        public Motor Motor { get; }
+
+        public Amphibian(int displacement, int numberOfWheels, Motor motor)
+        {
+            Displacement = displacement;
+            NumberOfWheels = numberOfWheels;
+            Motor = motor;
+
+            Environment = VehicleMovementEnvironment.Ground;
+        }
+
+        public void GetIntoWater()
+        {
+            Environment = VehicleMovementEnvironment.Water;
+        }
+
+        public void GetOntoGround()
+        {
+            Environment = VehicleMovementEnvironment.Ground;
         }
     }
 }
